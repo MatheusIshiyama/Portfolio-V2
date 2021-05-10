55c8e71f3b00c1737f3feb1e5556b16c949dc183
Matheus Ishiyama <matheus.ishiyama@outlook.com>
2021-05-10 15:14:54 -0300

    change: 'tecnologies' to 'skills'

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -41,7 +41,7 @@ export default function NavBar() {
                 </li>
                 <li>
                     <a href="#" onClick={handleToggle}>
-                        {t("tecnologies")}
+                        {t("skills")}
                     </a>
                 </li>
                 <li>
